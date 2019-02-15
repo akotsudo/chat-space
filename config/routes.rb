326b170cf4fa3ad78e79aec12da40672ca522565
akotsudo <akotsudo.1007@gmail.com>
2019-02-15 20:12:35 +0900

    change routes.rb
    routeのコンフリクト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-<<<<<<< HEAD
   resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-=======
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
->>>>>>> master
   end
 end
